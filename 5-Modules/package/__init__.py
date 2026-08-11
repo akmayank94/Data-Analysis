21ad6e74a44dfac45aae6313e046d8bba8d5e064
OpenCVProjectFiles <akmayank94@gmail.com>
2026-08-11 05:37:48 +0530

    feat(5-Modules): custom packages

---
+++ 5-Modules/package/__init__.py
@@ -0,0 +1,4 @@
+"""
+__init__.py is a special file in python used to define a package and initialize their namespace
+"""
+
---
+++ 5-Modules/package/maths.py
@@ -0,0 +1,5 @@
+# this maths.py is inside package folder so its a module now
+
+def add(a,b):
+    return a+b
+
